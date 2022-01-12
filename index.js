8102c467140800db69ee59ae2ecdf8914c84d586
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-01-13 05:06:41 +0800

    0113 完成

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const execSync = require('child_process').execSync
 
 let directory = path.resolve('./input/')
 let destination = path.resolve('./output/output.yaml')
+let destinationForDelete = path.resolve('./output/output-delete.yaml')
 
 function sleep(ms = 500) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 let main = async (eventType, filename) => {
-  
-  if (fs.existsSync(destination)) {
+  console.log(1)
+  if (fs.existsSync(destinationForDelete)) {
     try {
-      let outputBefore = execSync('kubectl.exe delete -f ' + destination
+      console.log(1, 'kubectl.exe delete -f ' + destinationForDelete)
+      
+      //execSync('kubectl delete pvc/mysql-hostpath-mysql-statefulset-0'
+      //  , { encoding: 'utf-8' });  // the default is 'buffer'
+      
+      let outputBefore = execSync('kubectl.exe delete -f ' + destinationForDelete
         , { encoding: 'utf-8' });  // the default is 'buffer'
       if (typeof(outputBefore) === 'string') {
         outputBefore = outputBefore.trim()
@@ -22,18 +28,63 @@ let main = async (eventType, filename) => {
       }
       console.log('[Wait for deleting...]')
       
-      
       let cleaned = false
       while (cleaned === false) {
         await sleep(1000)
         if (again) {
-          return false
+          again = false
+          return await main()
         }
         let cleanOutput = execSync('kubectl.exe get pod'
             , { encoding: 'utf-8' });  // the default is 'buffer'
         console.log(cleanOutput)
         if (cleanOutput.trim() === '') {
           cleaned = true
+          await sleep(1000)
+        }
+        else {
+          execSync('kubectl.exe delete -f ' + destinationForDelete
+            , { encoding: 'utf-8' });  // the default is 'buffer'
+        }
+      }
+      
+      cleaned = false
+      while (cleaned === false) {
+        await sleep(1000)
+        if (again) {
+          again = false
+          return await main()
+        }
+        let cleanOutput = execSync('kubectl.exe get pvc'
+            , { encoding: 'utf-8' });  // the default is 'buffer'
+        console.log(cleanOutput)
+        if (cleanOutput.trim() === '') {
+          cleaned = true
+          await sleep(1000)
+        }
+        else {
+          execSync('kubectl.exe delete -f ' + destinationForDelete
+            , { encoding: 'utf-8' });  // the default is 'buffer'
+        }
+      }
+      
+      cleaned = false
+      while (cleaned === false) {
+        await sleep(1000)
+        if (again) {
+          again = false
+          return await main()
+        }
+        let cleanOutput = execSync('kubectl.exe get pv'
+            , { encoding: 'utf-8' });  // the default is 'buffer'
+        console.log(cleanOutput)
+        if (cleanOutput.trim() === '') {
+          cleaned = true
+          await sleep(1000)
+        }
+        else {
+          execSync('kubectl.exe delete -f ' + destinationForDelete
+            , { encoding: 'utf-8' });  // the default is 'buffer'
         }
       }
       
@@ -48,6 +99,8 @@ let main = async (eventType, filename) => {
       console.error(e)
     }
   }
+  console.log(2)
+  
   //return false
   //console.log(eventType);
   // could be either 'rename' or 'change'. new file event and delete
@@ -56,6 +109,11 @@ let main = async (eventType, filename) => {
   fs.readdirAsync(directory)
     .map(file => fs.readFileAsync(path.join(directory, file), 'utf8'))
     .then(contents => fs.writeFileAsync(destination, contents.join('\n---\n')))
+  
+  fs.readdirAsync(directory)
+    .map(file => fs.readFileAsync(path.join(directory, file), 'utf8'))
+    .then(contents => fs.writeFileAsync(destinationForDelete, contents.reverse().join('\n---\n')))
+  
     
   //console.log(filename)
   try {
@@ -91,8 +149,9 @@ fs.watch(directory, () => {
   clearTimeout(timer1)
   clearTimeout(timer2)
   timer1 = setTimeout(() => {
-    console.log('prepare writing... wait for 3 seconds')
+    console.log('prepare writing... wait for 3 seconds', lock, again)
     timer2 = setTimeout(async () => {
+      console.log('lock', lock, again)
       if (lock === true) {
         again = true
         return false
